62d857a2168863e0cb485510e530d42603252c79
valclassdevelop <developervalclass.com>
2023-12-08 18:10:49 +0700

    remove options at mongoose

--- src/routes/accountRouters.js
+++ src/routes/accountRouters.js
@@ -3,26 +3,20 @@ const accountController = require('../controllers/accountControllers')
 const router = express.Router()
 
 // Sign consumer and seller
-router.post('/signup/seller', accountController.signUpSeller)
-router.post('/signin/seller', accountController.signInSeller)
-
-router.post('/signup/consumer', accountController.signUpConsumer)
-router.post('/signin/consumer', accountController.signInConsumer)
+router.post('/signup', accountController.signUpSeller)
+router.post('/signin', accountController.signInSeller)
 
 // Get list users
-router.get('/list/consumer/:consumer_id', accountController.getAllConsumer)
-router.get('/list/seller/:seller_id?', accountController.getAllSeller)
+router.get('/list/user/:user_id', accountController.getAllConsumer)
 
 // Delete Account
-router.delete('/consumer/:consumer_id', accountController.removeConsumer)
-router.delete('/seller/:seller_id', accountController.removeSeller)
+router.delete('/user/:user_id', accountController.removeSeller)
 
 // Update Account
-router.put('/consumer/:consumer_id', accountController.upload.single('consumer_image'), accountController.updateConsumerAccount)
-router.put('/seller/:seller_id', accountController.upload.single('seller_image'), accountController.updateSellerAccount)
+router.put('/user/:user_id', accountController.upload.single('seller_image'), accountController.updateSellerAccount)
 
 // Reset password
-router.post('/seller/forgot-password', accountController.forgotPassword)
-router.put('/seller/reset-password/:token', accountController.resetPassword)
+router.post('/user/forgot-password', accountController.forgotPassword)
+router.put('/user/reset-password/:token', accountController.resetPassword)
 
 module.exports = router
